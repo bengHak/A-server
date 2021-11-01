30429556cb966767813f490c1c6c01c61066fa1a
bengHak <qkwl4678@naver.com>
2021-11-01 15:07:39 +0900

    feat: ✨ Implement UserController

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,15 +1,119 @@
+const query = require("../query");
+const { getNow } = require("../lib");
+
 /* 
     관리자 뷰 데이터 조회
-    - 최근 7일 조회수 순
-    - 최근 댓글
-    - 최근 글
-    - 인기 글
+    - 최근 7일 글
+    - 최근 댓글 20개
+    - 최근 글 20개
+    - 글 20개 좋아요 순
 */
+exports.getAdminData = async (req, res) => {
+    const conn = await res.pool.getConnection();
+    const { user_id } = res;
+    const limit = 20;
+    let data = {
+        recent_7day_posts: [],
+        recent_comments_20: [],
+        recent_posts_20: [],
+        post_liked_by_user: [],
+    };
+
+    try {
+        // 최근 7일간 작성된 글
+        const recent_7day_posts = await conn.query(
+            query.admin.recent_7day_posts,
+            [user_id, getNow(), getNow()]
+        );
+        data.recent_7day_posts = recent_7day_posts[0];
+
+        // 최근 댓글 20개
+        const recent_comments_20 = await conn.query(
+            query.admin.recent_comments_20,
+            [user_id, getNow(), getNow()]
+        );
+        data.recent_comments_20 = recent_comments_20[0];
+
+        // 최근 글 20개
+        const recent_posts_20 = await conn.query(query.admin.recent_posts_20, [
+            user_id,
+            getNow(),
+            getNow(),
+        ]);
+        data.recent_posts_20 = recent_posts_20[0];
+
+        // 글 20개 좋아요 순
+        const post_liked_by_user = await conn.query(
+            query.admin.post_liked_by_user,
+            [user_id, getNow(), getNow()]
+        );
+        data.post_liked_by_user = post_liked_by_user[0];
+
+        res.json({
+            success: true,
+            data,
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            success: false,
+            message: "서버 오류",
+        });
+    } finally {
+        conn.release();
+    }
+};
+
+// 프로필 가져오기
+exports.getProfile = async (req, res) => {
+    const conn = await res.pool.getConnection();
+    const { user_id } = res;
+
+    try {
+        const profile = await conn.query(query.SELECT_USER_WITHOUT_PASSWORD, [
+            user_id,
+        ]);
+        res.json({
+            success: true,
+            data: profile[0][0],
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            success: false,
+            message: "서버 오류",
+        });
+    } finally {
+        conn.release();
+    }
+};
 
 /*
     프로필 수정
-    - 배경 이미지 설정
-    - 프로필 이미지 설정
+    - 이름 설정
     - 블로그 이름 설정
-    - 작가 이름 설정
 */
+exports.updateProfile = async (req, res) => {
+    const conn = await res.pool.getConnection();
+    const { user_id } = res;
+    const { name, blog_name } = req.body;
+
+    try {
+        await conn.query(query.UPDATE_USER_USERNAME_BLOG_TITLE, [
+            name,
+            blog_name,
+            user_id,
+        ]);
+        res.json({
+            success: true,
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            success: false,
+            message: "서버 오류",
+        });
+    } finally {
+        conn.release();
+    }
+};
--- query.js
+++ query.js
@@ -1,11 +1,15 @@
-// AUTH
+// USER
 exports.SELECT_USER = `SELECT * FROM users WHERE id = ?`;
+exports.SELECT_USER_WITHOUT_PASSWORD = `SELECT id, username, email, blog_title, profile_image_path, blog_image_path FROM users WHERE id = ?`;
 exports.SELECT_USER_BY_USERNAME = `SELECT * FROM users WHERE username = ?`;
 exports.SELECT_USER_BY_EMAIL = `SELECT * FROM users WHERE email = ?`;
 exports.SELECT_USER_BY_BLOG_TITLE = `SELECT * FROM users WHERE blog_title = ?`;
 exports.INSERT_USER = `INSERT INTO users (email, password, username, blog_title, profile_image_path, blog_image_path, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 exports.UPDATE_USER_USERNAME = `UPDATE users SET username = ? WHERE id = ?`;
 exports.UPDATE_USER_BLOG_TITLE = `UPDATE users SET blog_title = ? WHERE id = ?`;
+exports.UPDATE_USER_USERNAME_BLOG_TITLE = `UPDATE users SET username = ?, blog_title = ? WHERE id = ?`;
+exports.UPDATE_USER_PROFILE_IMAGE_PATH = `UPDATE users SET profile_image_path = ? WHERE id = ?`;
+exports.UPDATE_USER_BLOG_IMAGE_PATH = `UPDATE users SET blog_image_path = ? WHERE id = ?`;
 
 // POST
 exports.SELECT_POSTS_ORDERBY_CREATED_AT_PAGINATION = `SELECT * FROM posts ORDER BY created_at DESC LIMIT ?, ?`;
@@ -22,3 +26,10 @@ exports.INSERT_COMMENT = `INSERT INTO comments (user_id, post_id, content, creat
 exports.UPDATE_COMMENT = `UPDATE comments SET content = ?, updated_at = ? WHERE id = ?`;
 exports.SELECT_COMMENT = `SELECT * FROM comments WHERE id = ?`;
 exports.DELETE_COMMENT = `DELETE FROM comments WHERE id = ?`;
+
+exports.admin = {
+    recent_7day_posts: `SELECT * FROM posts WHERE created_at >= DATE_SUB(NOW(), INTERVAL 7 DAY) ORDER BY created_at DESC`,
+    recent_comments_20: `SELECT * FROM comments ORDER BY created_at DESC LIMIT 0, 20`,
+    recent_posts_20: `SELECT * FROM posts ORDER BY created_at DESC LIMIT 20`,
+    post_liked_by_user: `SELECT * FROM posts WHERE id IN (SELECT post_id FROM likes WHERE user_id = ?) ORDER BY created_at DESC LIMIT 20`,
+};
