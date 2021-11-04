29fc6de726875af133bd402e706e51cd99381376
bengHak <qkwl4678@naver.com>
2021-11-05 08:51:58 +0900

    refactor: 🛠 1차 마무리

--- controller/PostController.js
+++ controller/PostController.js
@@ -1,5 +1,5 @@
 const query = require("../query");
-const { getNow } = require("../lib");
+const { getNow, getTimestamp, getNowTimestamp } = require("../lib");
 
 // 전체 글 불러오기 (최신순, pagination)
 exports.getAllPosts = async (req, res) => {
@@ -10,22 +10,33 @@ exports.getAllPosts = async (req, res) => {
     try {
         const [rows] = await conn.query(
             query.SELECT_POSTS_ORDERBY_CREATED_AT_PAGINATION,
-            [limit, offset]
+            [offset, limit]
         );
 
         if (rows.length === 0) {
             res.status(404).json({
-                message: "글이 없습니다.",
+                success: true,
+                msg: "글이 없습니다.",
+                data: [],
             });
         } else {
+            let newRows = rows.map((e) => ({
+                id: e.id,
+                title: e.title,
+                content: e.content,
+                createdAt: getTimestamp(e.created_at),
+                updatedAt: getTimestamp(e.updated_at),
+                userId: e.user_id,
+            }));
             res.status(200).json({
-                message: "전체 글 불러오기 성공",
-                posts: rows,
+                success: true,
+                msg: "전체 글 불러오기 성공",
+                data: newRows,
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "get post error" });
+        res.status(500).json({ success: false, msg: "get post error" });
     } finally {
         conn.release();
     }
@@ -47,41 +58,45 @@ exports.getMyPosts = async (req, res) => {
 
         if (rows.length === 0) {
             res.status(404).json({
-                message: "글이 없습니다.",
+                success: true,
+                msg: "글이 없습니다.",
             });
         } else {
             res.status(200).json({
-                message: "내 글 불러오기 성공",
+                success: true,
+                msg: "내 글 불러오기 성공",
                 posts: rows,
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "get post error" });
+        res.status(500).json({ success: false, msg: "get post error" });
     } finally {
         conn.release();
     }
 };
 
 // 글 작성
-exports.postPost = async (req, res) => {
+exports.newPost = async (req, res) => {
     const conn = await res.pool.getConnection();
     const { title, content } = req.body;
     try {
         const now = await getNow();
         await conn.query(query.INSERT_POST, [
-            res.user_id,
             title,
             content,
+            res.user_id,
             now,
             now,
         ]);
         res.status(200).json({
-            message: "글 작성 성공",
+            success: true,
+            msg: "글 작성 성공",
+            data: "",
         });
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "post post error" });
+        res.status(500).json({ success: false, msg: "new post error" });
     } finally {
         conn.release();
     }
@@ -92,20 +107,31 @@ exports.getPost = async (req, res) => {
     const conn = await res.pool.getConnection();
     const { id } = req.params;
     try {
-        const [rows] = await conn.query(query.SELECT_POST, [post_id]);
+        const [rows] = await conn.query(query.SELECT_POST, [id]);
         if (rows.length === 0) {
             res.status(404).json({
-                message: "글이 없습니다.",
+                success: true,
+                msg: "글이 없습니다.",
             });
         } else {
+            let newRow = {
+                id: rows[0].id,
+                title: rows[0].title,
+                content: rows[0].content,
+                createdAt: getTimestamp(rows[0].created_at),
+                updatedAt: getTimestamp(rows[0].updated_at),
+                userId: rows[0].user_id,
+            };
+
             res.status(200).json({
-                message: "개별 글 불러오기 성공",
-                post: rows[0],
+                success: true,
+                msg: "개별 글 불러오기 성공",
+                data: newRow,
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "get post error" });
+        res.status(500).json({ msg: "get post error" });
     } finally {
         conn.release();
     }
@@ -116,24 +142,27 @@ exports.deletePost = async (req, res) => {
     const conn = await res.pool.getConnection();
     const { id } = req.params;
     try {
-        const [rows] = await conn.query(query.SELECT_POST, [post_id]);
+        const [rows] = await conn.query(query.SELECT_POST, [id]);
         if (rows.length === 0) {
             res.status(404).json({
-                message: "글이 없습니다.",
+                success: true,
+                msg: "글이 없습니다.",
             });
         } else if (rows[0].user_id !== res.user_id) {
             res.status(403).json({
-                message: "글 작성자만 삭제 가능합니다.",
+                success: false,
+                msg: "글 작성자만 삭제 가능합니다.",
             });
         } else {
-            await conn.query(query.DELETE_POST, [post_id]);
+            await conn.query(query.DELETE_POST, [id]);
             res.status(200).json({
-                message: "글 삭제 성공",
+                success: true,
+                msg: "글 삭제 성공",
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "delete post error" });
+        res.status(500).json({ success: false, msg: "delete post error" });
     } finally {
         conn.release();
     }
@@ -146,29 +175,32 @@ exports.updatePost = async (req, res) => {
     const { title, content } = req.body;
     try {
         const updated_at = await getNow();
-        const [rows] = await conn.query(query.SELECT_POST, [post_id]);
+        const [rows] = await conn.query(query.SELECT_POST, [id]);
         if (rows.length === 0) {
             res.status(404).json({
-                message: "글이 없습니다.",
+                success: false,
+                msg: "글이 없습니다.",
             });
         } else if (rows[0].user_id !== res.user_id) {
             res.status(403).json({
-                message: "글 작성자만 수정 가능합니다.",
+                success: true,
+                msg: "글 작성자만 수정 가능합니다.",
             });
         } else {
             await conn.query(query.UPDATE_POST, [
                 title,
                 content,
                 updated_at,
-                post_id,
+                id,
             ]);
             res.status(200).json({
-                message: "글 수정 성공",
+                success: true,
+                msg: "글 수정 성공",
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "update post error" });
+        res.status(500).json({ success: true, msg: "update post error" });
     } finally {
         conn.release();
     }
@@ -182,17 +214,19 @@ exports.getLikeCount = async (req, res) => {
         const [rows] = await conn.query(query.SELECT_POST, [post_id]);
         if (rows.length === 0) {
             res.status(404).json({
-                message: "글이 없습니다.",
+                success: false,
+                msg: "글이 없습니다.",
             });
         } else {
             res.status(200).json({
-                message: "글 좋아요 수 조회 성공",
+                success: true,
+                msg: "글 좋아요 수 조회 성공",
                 like_count: rows[0].like_count,
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "get like count error" });
+        res.status(500).json({ success: false, msg: "get like count error" });
     } finally {
         conn.release();
     }
@@ -207,17 +241,19 @@ exports.likePost = async (req, res) => {
         const now = await getNow();
         if (rows.length === 0) {
             res.status(404).json({
-                message: "글이 없습니다.",
+                success: false,
+                msg: "글이 없습니다.",
             });
         } else {
             await conn.query(query.LIKE_POST, [res.user_id, post_id, now]);
             res.status(200).json({
-                message: "글 좋아요 성공",
+                success: true,
+                msg: "글 좋아요 성공",
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "like post error" });
+        res.status(500).json({ success: false, msg: "like post error" });
     } finally {
         conn.release();
     }
@@ -231,17 +267,19 @@ exports.unlikePost = async (req, res) => {
         const [rows] = await conn.query(query.SELECT_POST, [post_id]);
         if (rows.length === 0) {
             res.status(404).json({
-                message: "글이 없습니다.",
+                success: false,
+                msg: "글이 없습니다.",
             });
         } else {
             await conn.query(query.UNLIKE_POST, [res.user_id, post_id]);
             res.status(200).json({
-                message: "글 좋아요 취소 성공",
+                success: true,
+                msg: "글 좋아요 취소 성공",
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "unlike post error" });
+        res.status(500).json({ success: false, msg: "unlike post error" });
     } finally {
         conn.release();
     }
@@ -252,23 +290,33 @@ exports.getComments = async (req, res) => {
     const conn = await res.pool.getConnection();
     const { id } = req.params;
     try {
-        const [rows] = await conn.query(query.SELECT_POST, [post_id]);
+        const [rows] = await conn.query(query.SELECT_POST, [id]);
         if (rows.length === 0) {
             res.status(404).json({
-                message: "글이 없습니다.",
+                success: false,
+                msg: "글이 없습니다.",
             });
         } else {
-            const [comments] = await conn.query(query.SELECT_COMMENTS, [
-                post_id,
+            const [comments] = await conn.query(query.SELECT_COMMENT_AND_USER, [
+                id,
             ]);
+            let newComments = comments.map((comment) => ({
+                commentId: comment.id,
+                userId: comment.user_id,
+                username: comment.username,
+                postId: comment.post_id,
+                content: comment.content,
+                createdAt: getTimestamp(comment.created_at),
+            }));
             res.status(200).json({
-                message: "댓글 불러오기 성공",
-                comments: comments,
+                success: true,
+                msg: "댓글 불러오기 성공",
+                data: newComments,
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "get comments error" });
+        res.status(500).json({ success: false, msg: "get comments error" });
     } finally {
         conn.release();
     }
@@ -283,16 +331,18 @@ exports.writeComment = async (req, res) => {
         const now = await getNow();
         await conn.query(query.INSERT_COMMENT, [
             res.user_id,
-            post_id,
+            id,
             content,
+            now,
             now,
         ]);
         res.status(200).json({
-            message: "댓글 작성 성공",
+            success: true,
+            msg: "댓글 작성 성공",
         });
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "write comment error" });
+        res.status(500).json({ success: false, msg: "write comment error" });
     } finally {
         conn.release();
     }
@@ -307,17 +357,19 @@ exports.updateComment = async (req, res) => {
         const now = await getNow();
         if (res.user_id !== res.comment_user_id) {
             res.status(403).json({
-                message: "댓글 작성자만 수정 가능합니다.",
+                success: false,
+                msg: "댓글 작성자만 수정 가능합니다.",
             });
         } else {
             await conn.query(query.UPDATE_COMMENT, [content, now, comment_id]);
             res.status(200).json({
-                message: "댓글 수정 성공",
+                success: true,
+                msg: "댓글 수정 성공",
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "update comment error" });
+        res.status(500).json({ success: false, msg: "update comment error" });
     } finally {
         conn.release();
     }
@@ -326,26 +378,30 @@ exports.updateComment = async (req, res) => {
 // 댓글 삭제
 exports.deleteComment = async (req, res) => {
     const conn = await res.pool.getConnection();
-    const { id } = req.params;
+    const { commentId } = req.params;
     try {
-        const [rows] = await conn.query(query.SELECT_COMMENT, [comment_id]);
+        const [rows] = await conn.query(query.SELECT_COMMENT, [commentId]);
+        console.log(rows);
         if (rows.length === 0) {
             res.status(404).json({
-                message: "댓글이 없습니다.",
+                success: false,
+                msg: "댓글이 없습니다.",
             });
-        } else if (res.user_id !== res.comment_user_id) {
+        } else if (res.user_id !== rows[0]["user_id"]) {
             res.status(403).json({
-                message: "댓글 작성자만 삭제 가능합니다.",
+                success: false,
+                msg: "댓글 작성자만 삭제 가능합니다.",
             });
         } else {
-            await conn.query(query.DELETE_COMMENT, [comment_id]);
+            await conn.query(query.DELETE_COMMENT, [commentId]);
             res.status(200).json({
-                message: "댓글 삭제 성공",
+                success: true,
+                msg: "댓글 삭제 성공",
             });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "delete comment error" });
+        res.status(500).json({ success: false, msg: "delete comment error" });
     } finally {
         conn.release();
     }
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ var router = express.Router();
 
 router.get("/admin", verifyToken, user.getAdminData); // 블로그 통계 가져오기
 
+router.get("/:id/profile", user.getProfileById); // 프로필 가져오기
+
 router.get("/me/profile", verifyToken, user.getProfile); // 유저 프로필 가져오기
 
 router.post("/me/profile", verifyToken, user.updateProfile); // 프로필 수정 하기
